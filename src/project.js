84db3a59ec01648938757798606226856966fb52
Florrie <towerofnix@gmail.com>
2019-01-30 20:49:56 -0400

    More project details

--- src/project.js
+++ src/project.js
@@ -1,6 +1,16 @@
 const APIDocument = require('./util/api-document');
 
 /**
+ * Object containing information about project community stats.
+ * @typedef {object} Project~ProjectStats
+ * @property {number} views - Number of views (unique users who have clicked the green flag to start the project).
+ * @property {number} loves - Number of love-its.
+ * @property {number} favorites - Number of favorites.
+ * @property {number} comments - Number of comments left on the project.
+ * @property {number} remixes - Number of shared remixes.
+ */
+
+/**
  * Class representing a Scratch project.
  */
 class Project extends APIDocument {
@@ -45,6 +55,74 @@ class Project extends APIDocument {
      */
     async getAuthor() {
         return this.getAPIDetail('author').username;
+    }
+
+    /**
+     * Gets the project's "Instructions" content.
+     * @returns {string}
+     */
+    async getInstructions() {
+        return this.getAPIDetail('instructions');
+    }
+
+    /**
+     * Gets the project's "Notes and Credits" content.
+     * @returns {string}
+     */
+    async getNotesAndCredits() {
+        return this.getAPIDetail('description');
+    }
+
+    /**
+     * Gets the project's various stats - number of love-its, comments, etc.
+     * @returns {Project~ProjectStats}
+     */
+    async getStats() {
+        const { views, loves, favorites, comments, remixes } = await this.getAPIDetail('stats');
+        return {views, loves, favorites, comments, remixes};
+    }
+
+    /**
+     * Gets the URL to be used as the thumbnail for the project.
+     * @returns {string}
+     */
+    async getThumbnailURL() {
+        return this.getAPIDetail('image');
+    }
+
+    /**
+     * Gets the date/time when the project was created.
+     * @returns {Date}
+     */
+    async getCreateDate() {
+        return this._getDate('created');
+    }
+
+    /**
+     * Gets the date/time when the project was last modified.
+     * @returns {Date}
+     */
+    async getModifyDate() {
+        return this._getDate('modified');
+    }
+
+    /**
+     * Gets the date/time when the project was shared.
+     * @returns {Date}
+     */
+    async getShareDate() {
+        return this._getDate('shared');
+    }
+
+    /**
+     * Helper function to retrieve a date related to the project.
+     * @param {string} key - Property to get from history API detail.
+     * @returns {Date}
+     * @private
+     */
+    async _getDate(key) {
+        const history = await this.getAPIDetail('history');
+        return new Date(history[key]);
     }
 }
 
--- test/project.js
+++ test/project.js
@@ -12,17 +12,70 @@ t.test('API endpoint', t => {
     t.done();
 });
 
+t.test('_getDate', async t => {
+    const fakeDate = '2019-01-31T00:00:00.000Z';
+    const history = {fakeDate};
+
+    const project = new Project({id: 1});
+
+    let getAPIDetailCalled = false;
+
+    project.getAPIDetail = key => {
+        getAPIDetailCalled = true;
+        t.is(key, 'history');
+        return Promise.resolve(history);
+    };
+
+    const result = await project._getDate('fakeDate');
+    t.true(getAPIDetailCalled);
+    t.true(result instanceof Date);
+    t.is(result.toISOString(), fakeDate);
+});
+
+t.test('_getDate-based API details', async t => {
+    const project = new Project({id: 1});
+
+    let expectedKey;
+    let calledGetDate;
+
+    project._getDate = key => {
+        calledGetDate = true;
+        t.is(key, expectedKey);
+        return Promise.resolve(new Date());
+    };
+
+    const testDateDetail = async (key, func) => {
+        expectedKey = key;
+        calledGetDate = false;
+        const result = await func.call(project);
+        t.true(calledGetDate);
+        t.true(result instanceof Date);
+    };
+
+    await testDateDetail('created', project.getCreateDate);
+    await testDateDetail('modified', project.getModifyDate);
+    await testDateDetail('shared', project.getShareDate);
+});
+
 t.test('Basic API details', async t => {
     const id = 1;
     const title = 'Our Project';
     const username = 'mres';
+    const description = 'Apple banana.';
+    const instructions = 'Space bar!';
+    const stats = {views: 100, loves: 8, favorites: 6, comments: 23, remixes: 2};
+    const image = 'https://example.com';
 
     const apiDetails = {
         id,
         title,
+        description,
+        instructions,
         author: {
             username
-        }
+        },
+        stats,
+        image
     };
 
     const project = new Project({id});
@@ -32,4 +85,8 @@ t.test('Basic API details', async t => {
     t.is(await testDetail('id', project.getID), id);
     t.is(await testDetail('title', project.getTitle), title);
     t.is(await testDetail('author', project.getAuthor), username);
+    t.is(await testDetail('instructions', project.getInstructions), instructions);
+    t.is(await testDetail('description', project.getNotesAndCredits), description);
+    t.match(await testDetail('stats', project.getStats), stats);
+    t.is(await testDetail('image', project.getThumbnailURL), image);
 });
